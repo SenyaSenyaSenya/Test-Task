b3e3ab51628e27be3fa9a34b68fa532c601835dd
SenyaSenyaSenya <kseniamakareckaa@gmail.com>
2023-11-23 22:27:02 +0300

    23

--- app/src/main/java/com/example/testtask/ui/HomeScreenActivity.kt
+++ app/src/main/java/com/example/testtask/ui/HomeScreenActivity.kt
@@ -1,18 +1,19 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.testtask.ui
 
 import BottomNavigationHelper
 import PexelsPhotoAdapter
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.Menu
-import android.widget.AbsListView
-import android.widget.EditText
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.testtask.R
@@ -23,15 +24,16 @@ import com.example.testtask.viewmodels.ViewModelFactory
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeScreenActivity : AppCompatActivity() {
-    private lateinit var gridLayoutManager: GridLayoutManager
     private lateinit var adapter: PexelsPhotoAdapter
     private lateinit var recyclerView: RecyclerView
+    private lateinit var searchEditText: EditText
     var isScrolling = false
     var isPhotoSearch = false
     var currentItems = 0
     var totalItems = 0
     var scrollOutItems = 0
     var searchQuery = String()
+    private lateinit var tryAgainTextView: TextView
     private lateinit var viewModel: PexelsViewModel
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var bottomNavigationHelper: BottomNavigationHelper
@@ -42,15 +44,22 @@ class HomeScreenActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recyclerView)
         val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         recyclerView.layoutManager = layoutManager
-        val application = requireNotNull(this).application
         val factory = ViewModelFactory()
         viewModel = ViewModelProviders.of(this, factory).get(PexelsViewModel::class.java)
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
         bottomNavigationHelper = BottomNavigationHelper(this, bottomNavigationView)
+        tryAgainTextView = findViewById<TextView>(R.id.tryAgainTextView)
         initialiseAdapter()
         requestForData(false, "", false)
-        val searchEditText = findViewById<EditText>(R.id.editText)
+        searchEditText = findViewById<EditText>(R.id.editText)
         EditTextUtils.addClearButton(searchEditText)
+        tryAgainTextView.setOnClickListener {
+            if (isPhotoSearch) {
+                requestForData(true, searchQuery, false)
+            } else {
+                requestForData(false, "", false)
+            }
+        }
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -100,6 +109,7 @@ class HomeScreenActivity : AppCompatActivity() {
         }
     }
 
+
     private fun initialiseAdapter() {
         val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         recyclerView.layoutManager = layoutManager
@@ -109,21 +119,32 @@ class HomeScreenActivity : AppCompatActivity() {
     private fun observeData() {
         adapter = PexelsPhotoAdapter(this, viewModel.arrayList)
         recyclerView.adapter = adapter
+
+        val noResultsLayout = findViewById<LinearLayout>(R.id.no_results)
+        if (viewModel.arrayList.isEmpty()) {
+            noResultsLayout.visibility = View.VISIBLE
+        } else {
+            noResultsLayout.visibility = View.GONE
+        }
     }
 
     private fun requestForData(isSearch: Boolean, searchQuery: String, isAction: Boolean) {
         if (NetworkUtils.isNetworkAvailable(this)) {
             try {
                 viewModel.getPexelsData(this, isSearch, searchQuery, isAction, adapter)
+                tryAgainTextView.visibility = View.GONE // Скрываем tryAgainTextView, если интернет-соединение восстановлено
             } catch (e: Exception) {
                 val errorMessage = "Network request failed: ${e.message}"
                 Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
             }
         } else {
+            tryAgainTextView.visibility = View.VISIBLE
             Toast.makeText(this, "No Internet Connection", Toast.LENGTH_LONG).show()
         }
     }
-
+    fun clearSearch(view: View) {
+        searchEditText.text.clear()
+    }
     @SuppressLint("ResourceType")
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.xml.search_menu, menu)
